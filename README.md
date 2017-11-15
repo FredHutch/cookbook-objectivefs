26fadb98c8f2f76750047f244f03385a5c379d41
btb-hutch <mrg+btb@fredhutch.org>
2017-11-14 17:27:21 -0800

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# objectivefs
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: objectivefs
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
