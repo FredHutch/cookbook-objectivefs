d7c80d9a06bff6cbde2d3d2fec68ae640ccb508f
Michael Gutteridge <michael.gutteridge@gmail.com>
2017-11-15 17:33:23 -0800

    Fixed cookbook metadata

--- metadata.rb
+++ metadata.rb
@@ -1,20 +1,11 @@
 name 'objectivefs'
-maintainer 'Hutch Build Automator'
-maintainer_email 'mrg+btb@fredhutch.org'
-license 'All Rights Reserved'
-description 'Installs/Configures objectivefs'
+maintainer 'Michael Gutteridge'
+maintainer_email 'mrg@fredhutch.org'
+license 'MIT'
+description 'Cookbook for ObjectiveFS'
 long_description 'Installs/Configures objectivefs'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 supports 'ubuntu'
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
 issues_url 'https://github.com/fredhutch/objectivefs/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
 source_url 'https://github.com/fredhutch/objectivefs'
