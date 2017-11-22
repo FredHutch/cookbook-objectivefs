4e3b908f3b2e6578746a261da66e9ff818fe2b63
Michael Gutteridge <michael.gutteridge@gmail.com>
2017-11-22 10:37:18 -0800

    Release 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mrg@fredhutch.org'
 license 'Apache 2.0'
 description 'Cookbook for ObjectiveFS'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.2'
+version '0.2.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 supports 'ubuntu'
 issues_url 'https://github.com/fredhutch/objectivefs/issues'
